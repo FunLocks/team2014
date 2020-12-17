a9291e9498638e1249b0449ca5093a5fbcff00e1
Sonoka927 <b1019008@fun.ac.jp>
2020-12-17 21:07:00 +0900

    create 4F

--- MotyokoiApplication/app/src/main/java/com/example/motyokoiapplication/ui/home/HomeViewModel.kt
+++ MotyokoiApplication/app/src/main/java/com/example/motyokoiapplication/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "食堂の収容人数　38人/89人"
     }
     val text: LiveData<String> = _text
 }
