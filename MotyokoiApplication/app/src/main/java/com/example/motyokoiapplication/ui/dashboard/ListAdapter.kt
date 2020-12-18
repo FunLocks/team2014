a23f2379576288d4b35cbe2582017d57f9021e5c
Sonoka927 <b1019008@fun.ac.jp>
2020-12-18 16:32:11 +0900

    先生の在室不在のimageをセットした

--- MotyokoiApplication/app/src/main/java/com/example/motyokoiapplication/ui/dashboard/ListAdapter.kt
+++ MotyokoiApplication/app/src/main/java/com/example/motyokoiapplication/ui/dashboard/ListAdapter.kt
@@ -14,21 +14,18 @@ import com.example.motyokoiapplication.R
 class ListAdapter(val context: Context, val UserList: ArrayList<User>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val user = UserList[position]
         val view: View = LayoutInflater.from(context).inflate(R.layout.item_user, null)
         val Name = view.findViewById<TextView>(R.id.name_tv)
+        Name?.text = user.name
         val Email = view.findViewById<TextView>(R.id.email_tv)
+            Email?.text = user.email
         val Place =  view.findViewById<TextView>(R.id.place_tv)
+            Place?.text = user.place
         var imageView = view?.findViewById<ImageView>(R.id.imageIcon)
-        imageView?.setImageResource(User.imageId)
+        imageView?.setImageResource(user.imageId)
         
-        val user = UserList[position]
-
         
-
-        Name.text = user.name
-        Email.text = user.email
-        Place.text = user.place
-
         
         return view
     }
